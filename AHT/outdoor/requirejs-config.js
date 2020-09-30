c6e3524d7a7369248ece459e515733835e39f269
dqtrinh <dqtrinh2410@gmail.com>
2020-09-30 16:41:25 +0700

    theme oe

---
+++ AHT/outdoor/requirejs-config.js
@@ -0,0 +1,17 @@
+var config = {
+    paths: {
+        slick: 'js/slick',
+    },
+    shim: { 
+        slick: {
+            deps: ['jquery']
+        }
+    },
+    // map: {
+    //     '*': {
+    //         qtyInput:'js/widgets/qty-input',
+    //         'Magento_Checkout/js/view/minicart': 'js/view/minicart',
+    //         'sidebar': 'js/sidebar'
+    //     }
+    // }
+};
